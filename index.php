da6a8c04fcfa718a90ab10e45ba7da0e7c6d5bc2
RomanKopyl <kopylroman@gmail.com>
2018-12-08 21:39:04 +0200

    First programm

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
+require_once 'vendor/autoload.php';
 
-echo 'Hello';
+$token = 'токен';
+$bot = new \TelegramBot\Api\Client($token);
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// команда для помощи
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - вывод справки';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
