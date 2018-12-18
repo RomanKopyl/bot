c728bc7737ede9a7d85907d92de36eb345f0696e
vagrant <vagrant@homestead>
2018-12-18 10:13:07 +0000

    Procfile for Heroku

--- routes/botman.php
+++ routes/botman.php
@@ -11,6 +11,15 @@ $botman->hears('How are you', function ($bot) {
     $bot->reply('I\'m fine, my Lord!');
 });
 $botman->hears('play', function ($bot) {
-    $bot->reply('I\'m fine, my Lord!');
+    $bot->reply('I\'m not player!');
+});
+$botman->hears('show', function ($bot) {
+    $products = App\Product::all();
+
+    foreach ($products as $product) {
+        $bot->reply(
+            '['.$product->id.'] '
+            .$product->name);
+    }
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
